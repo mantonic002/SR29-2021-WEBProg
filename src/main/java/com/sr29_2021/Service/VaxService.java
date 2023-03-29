5a1e936cb00281f3ebe4d1a2af108ecadf698586
mantonic002 <mantonic002@gmail.com>
2023-03-29 22:07:06 +0200

    Vax service

---
+++ src/main/java/com/sr29_2021/Service/VaxService.java
@@ -0,0 +1,30 @@
+package com.sr29_2021.Service;
+
+import com.sr29_2021.Model.Vax;
+import com.sr29_2021.Repository.IVaxRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VaxService {
+
+    private IVaxRepository repo;
+
+    public List<Vax> listAll() { return(List<Vax>) repo.findAll(); }
+
+    public void save(Vax vax) {
+        if(repo.findOne(vax.getId()) != null){
+            repo.update(vax);
+        } else{
+            repo.save(vax);
+        }
+    }
+
+    public Vax get(Integer id) {
+        Vax vax = repo.findOne(id);
+        return vax;
+    }
+
+    public void delete(Integer id) { repo.delete(id); }
+}
